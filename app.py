76ed41f90196bc19e2fa7ea1064353e76d4cfede
ADCoinX <muhdyusriadib@gmail.com>
2025-07-17 17:12:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def index():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)  # Render listens on $PORT; leaving default works too
