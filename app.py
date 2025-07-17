fc35065af0ace7ce94c683be33d87a51201281e7
ADCoinX <muhdyusriadib@gmail.com>
2025-07-18 01:30:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def index():
         result = validate_wallet(address)
     return render_template("index.html", result=result)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
