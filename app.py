185e75730df3b633a03702d625913a1dfb0c4c8f
ADCoinX <muhdyusriadib@gmail.com>
2025-07-22 00:10:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,6 @@ def validate():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
