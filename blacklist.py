e10a326870da0900c848cff445925a6b33bb0c82
ADCoinX <muhdyusriadib@gmail.com>
2025-07-30 14:22:12 +0800

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -5,6 +5,8 @@ def is_blacklisted(address):
     blacklist = [
         "0xScamExample1234567890",
         "TScammer987654321",
-        "1FAKESCAMBTC123456"
+        "1FAKESCAMBTC123456",
+        "TQWF3xEn44UrAS9a2bn7LKNssb1UbDeAx6",
+        "TQooBX9o8iSSprLWW96YShBogx7Uwisuim"
     ]
     return address.strip() in blacklist
